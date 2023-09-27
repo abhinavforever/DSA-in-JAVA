ae8fbf6fab6069ef8681b40569ac22869f3f3d3a
Abhinav <abhinavsri204@gmail.com>
2023-09-27 23:29:55 +0530

    2

---
+++ Sorting/unionOfTwoSortedArrays_NAIVE.java
@@ -0,0 +1,26 @@
+package Sorting;
+import java.util.Arrays;
+public class unionOfTwoSortedArrays {
+    static void printUnion(int a[],int b[]){
+        int m=a.length;
+        int n=b.length;
+        int c[]=new int[m+n];
+        for (int i=0;i<m;i++){
+            c[i]=a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            c[m+i]=b[i];
+        }
+        Arrays.sort(c);
+        for (int i = 0; i < m+n; i++) {
+            if(i==0 || c[i]!=c[i-1])
+                System.out.print(c[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[]={10,20,20};
+        int b[]={5,20,40,40};
+        printUnion(a,b);
+    }
+}
