74b7462900d18bb364d972bf1edfc6adac404f17
Abhinav <abhinavsri204@gmail.com>
2023-10-15 23:59:33 +0530

    day 37

---
+++ Linked_List/Linked_list_problems/Reverse_a_ll_iterative.java
@@ -0,0 +1,37 @@
+package Linked_List.Linked_list_problems;
+
+import java.util.Scanner;
+
+public class Reverse_a_ll {
+    static Node reverse(Node head){
+        Node curr=head;
+        Node prev=null;
+        while (curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;//prev is new head
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        Node temp1=new Node(20);
+        Node temp2=new Node(30);
+        head.next=temp1;
+        temp1.next=temp2;
+        Print(head);
+        System.out.println();
+        head=reverse(head);
+        Print(head);
+    }
+    static void Print(Node head) {
+        Node cur = head;
+        while(cur!=null){
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+
+    }
+}
---
+++ Linked_List/Linked_list_problems/remove_duplicates_sortedLL.java
@@ -0,0 +1,2 @@
+package Linked_List.Linked_list_problems;public class remove_duplicates_sortedLL {
+}
