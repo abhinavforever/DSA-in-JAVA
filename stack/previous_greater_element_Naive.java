2df5157ef237a6166a33775354f43d7263956eed
Abhinav <abhinavsri204@gmail.com>
2023-10-22 14:26:57 +0530

    day 43

---
+++ stack/previous_greater_element_Naive.java
@@ -0,0 +1,20 @@
+package stack;
+public class previous_greater_element {
+    static void printPrevGreater(int arr[],int n){
+        for(int i=0;i<n;i++){
+            int j;
+            for( j=i-1;j>=0;j--){
+                if(arr[j]>arr[i]){
+                    System.out.print(arr[j]+" ");
+                    break;
+                }
+            }
+            if(j==-1)
+                System.out.print(-1+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={20,30,10,5,15};
+        printPrevGreater(arr,arr.length);
+    }
+}
---
+++ stack/previous_greater_element_efficient.java
@@ -0,0 +1,2 @@
+package stack;public class previous_greater_element_efficient {
+}
