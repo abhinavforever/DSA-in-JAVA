a63611d4e77cfc8a4b5d5a3b792bb772dd9c45a0
Abhinav <abhinavsri204@gmail.com>
2023-10-25 22:19:18 +0530

    day 45

--- stack/Evaluation_of_Prefix.java
+++ stack/Evaluation_of_Prefix.java
@@ -1,2 +1,53 @@
-package stack;public class Evaluation_of_Prefix {
+package stack;
+import java.util.Stack;
+
+public class Evaluation_of_Prefix {
+    // Function to evaluate a prefix expression
+    public static int evaluatePrefix(String prefix) {
+        Stack<Integer> operandStack = new Stack<>();
+
+        for (int i = prefix.length() - 1; i >= 0; i--) {
+            char currentChar = prefix.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                operandStack.push(Character.getNumericValue(currentChar));
+            } else if (currentChar == ' ') {
+                continue; // Skip spaces
+            } else {
+                int operand1 = operandStack.pop();
+                int operand2 = operandStack.pop();
+                int result;
+
+                switch (currentChar) {
+                    case '+':
+                        result = operand1 + operand2;
+                        break;
+                    case '-':
+                        result = operand1 - operand2;
+                        break;
+                    case '*':
+                        result = operand1 * operand2;
+                        break;
+                    case '/':
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid operator: " + currentChar);
+                }
+                operandStack.push(result);
+            }
+        }
+
+        if (operandStack.size() != 1) {
+            throw new IllegalArgumentException("Invalid prefix expression");
+        }
+
+        return operandStack.pop();
+    }
+
+    public static void main(String[] args) {
+        String prefixExpression = "+ 5 * 3 2"; // Example prefix expression
+        int result = evaluatePrefix(prefixExpression);
+        System.out.println("Prefix Expression: " + prefixExpression);
+        System.out.println("Result: " + result);
+    }
 }
--- stack/infix_to_prefix.java
+++ stack/infix_to_prefix.java
@@ -1,2 +1,68 @@
-package stack;public class infix_to_prefix {
+package stack;
+import java.util.Stack;
+public class infix_to_prefix {
+    // Function to check if a character is an operator
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+    }
+    // Function to determine the precedence of an operator
+    private static int precedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+    // Function to convert infix to prefix
+    public static String infixToPrefix(String infix) {
+        Stack<Character> operatorStack = new Stack<>();
+        Stack<String> operandStack = new Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char currentChar = infix.charAt(i);
+            if (Character.isLetterOrDigit(currentChar)) {
+                operandStack.push(String.valueOf(currentChar));
+            } else if (currentChar == '(') {
+                operatorStack.push(currentChar);
+            } else if (currentChar == ')') {
+                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                    String operand2 = operandStack.pop();
+                    String operand1 = operandStack.pop();
+                    char operator = operatorStack.pop();
+                    String result = operator + operand1 + operand2;
+                    operandStack.push(result);
+                }
+                operatorStack.pop(); // Pop the '('
+            } else if (isOperator(currentChar)) {
+                while (!operatorStack.isEmpty() && precedence(currentChar) <= precedence(operatorStack.peek())) {
+                    String operand2 = operandStack.pop();
+                    String operand1 = operandStack.pop();
+                    char operator = operatorStack.pop();
+                    String result = operator + operand1 + operand2;
+                    operandStack.push(result);
+                }
+                operatorStack.push(currentChar);
+            }
+        }
+        while (!operatorStack.isEmpty()) {
+            String operand2 = operandStack.pop();
+            String operand1 = operandStack.pop();
+            char operator = operatorStack.pop();
+            String result = operator + operand1 + operand2;
+            operandStack.push(result);
+        }
+        return operandStack.pop();
+    }
+    public static void main(String[] args) {
+        String infixExpression = "(A+B)*C";
+        String prefixExpression = infixToPrefix(infixExpression);
+        System.out.println("Infix Expression: " + infixExpression);
+        System.out.println("Prefix Expression: " + prefixExpression);
+    }
 }
