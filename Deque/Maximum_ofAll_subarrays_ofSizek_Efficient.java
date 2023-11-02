ec77683332236f0289c3ba7c27b7788779d4c21e
Abhinav <abhinavsri204@gmail.com>
2023-11-02 20:29:42 +0530

    day 53

--- Deque/Maximum_ofAll_subarrays_ofSizek_Efficient.java
+++ Deque/Maximum_ofAll_subarrays_ofSizek_Efficient.java
@@ -1,2 +1,32 @@
-package Deque;public class Maximum_ofAll_subarrays_ofSizek_Efficient {
+package Deque;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Maximum_ofAll_subarrays_ofSizek_Efficient {
+    static void printMax(int arr[],int k){
+        int n=arr.length;
+        Deque<Integer> dq=new LinkedList<Integer>();
+        for(int i=0;i<k;i++){
+            while (!dq.isEmpty() && arr[i]>=arr[dq.peekLast()]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+
+        for(int i=k;i<n;i++){
+            System.out.println(arr[dq.peek()]+" ");
+            while (!dq.isEmpty() && dq.peek()<=i-k)
+                dq.removeFirst();
+            while (!dq.isEmpty() && arr[i]>=arr[dq.peekLast()])
+                dq.removeLast();
+            dq.addLast(i);
+        }
+        System.out.println(arr[dq.peek()]+" ");
+    }
+
+    public static void main(String[] args) {
+        int arr[]={20,40,30,10,60};
+        printMax(arr,3);
+    }
 }
--- Deque/Maximum_ofAll_subarrays_ofSizek_Naive.java
+++ Deque/Maximum_ofAll_subarrays_ofSizek_Naive.java
@@ -1,5 +1,18 @@
 package Deque;
 
-public class Maximum_ofAll_subarrays_ofSizek {
-    
+public class Maximum_ofAll_subarrays_ofSizek_Naive {
+    static void printMax(int arr[],int n,int k){
+        for(int i=0;i<n-k+1;i++){
+            int mx=arr[i];
+            for(int j=i+1;j<i+k;j++){
+                mx=Math.max(arr[j],mx);
+            }
+            System.out.println(mx);
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[]={10,8,5,12,15,7,6};
+        printMax(a,a.length,3);
+    }
 }
