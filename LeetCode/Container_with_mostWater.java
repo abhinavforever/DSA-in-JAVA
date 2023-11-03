b7b5cb54033eefae9c3f759364ebc86208dc20f7
Abhinav <abhinavsri204@gmail.com>
2023-11-04 01:32:58 +0530

    day 54

--- LeetCode/Container_with_mostWater.java
+++ LeetCode/Container_with_mostWater.java
@@ -1,2 +1,31 @@
-package LeetCode;public class Container_with_mostWater {
+package LeetCode;
+
+public class Container_with_mostWater {
+    //My Earlier Naive Approach :
+     public static int maxArea(int[] height) {
+         int n=height.length;
+         int area;
+         int max=Integer.MIN_VALUE;
+         for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                 if (height[i]<height[j]){
+                     area=height[i]*Math.abs(j-i);
+                 }
+                 else{
+                     area=height[j]*Math.abs(j-i);
+                 }
+                 if (area>max){
+                     max=area;
+                 }
+             }
+         }
+     return max;}
+
+    public static void main(String[] args) {
+         int height[]= {1,8,6,2,5,4,8,3,7};
+         System.out.println(maxArea(height));
+
+         int height2[]={1,1};
+        System.out.println(maxArea(height2));
+    }
 }
