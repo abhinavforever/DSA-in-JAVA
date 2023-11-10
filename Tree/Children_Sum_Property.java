eca259c815d8421adcc0622d920d23ce60073f3f
Abhinav <abhinavsri204@gmail.com>
2023-11-10 22:02:08 +0530

    day 61

--- Tree/Children_Sum_Property.java
+++ Tree/Children_Sum_Property.java
@@ -1,2 +1,23 @@
-package Tree;public class Children_Sum_Property {
-}
+package Tree;
+public class Children_Sum_Property {
+    static boolean isCsum(Node root){
+        if (root==null)
+            return true;
+        if(root.left==null && root.right==null)
+            return true;
+        int sum=0;
+        if(root.left!=null){
+            sum+=root.left.key;
+        }
+        if(root.right!=null){
+            sum+=root.right.key;
+        }
+        return (root.key==sum && isCsum(root.left) && isCsum(root.right));
+    }
+    public static void main(String[] args) {
+        Node root=new Node(20);
+        root.left=new Node(8);
+        root.right=new Node(12);
+        root.right.left=new Node(3);
+        root.right.right=new Node(9);
+        System.out.println(isCsum(root)); }}
