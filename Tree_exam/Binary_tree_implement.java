1449ae93e4664f37df51274f0bc239f807b4fe07
Abhinav <abhinavsri204@gmail.com>
2023-11-21 10:23:00 +0530

    day 71

---
+++ Tree_exam/Binary_tree_implement.java
@@ -0,0 +1,19 @@
+package Tree_exam;
+class Node{
+    int key;
+    Node left;
+    Node right;
+    Node (int k){
+        key=k;
+    }
+}
+public class Binary_tree_implement {
+    public static void main(String[] args) {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.right=new Node(30);
+        root.left.left=new Node(40);
+        System.out.println(root.left.left.key);
+        System.out.println(root.right.left);
+    }
+}
