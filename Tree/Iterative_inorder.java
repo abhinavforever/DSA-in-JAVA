598014e8e960e9b2ffa4136f0f0ffa32a75ab6b6
Abhinav <abhinavsri204@gmail.com>
2023-11-23 00:05:12 +0530

    day 72

--- Tree/Iterative_inorder.java
+++ Tree/Iterative_inorder.java
@@ -1,2 +1,24 @@
-package Tree;public class iterative_Inorder {
-}
+package Tree;
+
+import java.util.Stack;
+public class Iterative_inorder {
+    static void iterativeInorder(Node root){
+        Stack<Node> st=new Stack<>();
+        Node curr=root;
+        while (curr!=null || st.isEmpty()==false){
+            while (curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }
+            curr=st.pop();
+            System.out.print(curr.key+" ");
+            curr=curr.right;
+        }
+    }
+    public static void main(String[] args) {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.left.left=new Node(40);
+        root.left.right=new Node(50);
+        root.right=new Node(30);
+        iterativeInorder(root); }}
--- Tree/Iterative_preorder.java
+++ Tree/Iterative_preorder.java
@@ -1,2 +1,26 @@
-package Tree;public class Iterative_preorder {
+package Tree;
+import java.util.Stack;
+public class Iterative_preorder {
+    static void iterativePreorder(Node root){
+        if(root==null)
+            return;
+        Stack<Node> st=new Stack<>();
+        st.push(root);
+        while (st.isEmpty()==false){
+            Node curr=st.pop();
+            System.out.print(curr.key+" ");
+            if(curr.right!=null)
+                st.push(curr.right);
+            if(curr.left!=null)
+                st.push(curr.left);
+        }
+    }
+    public static void main(String[] args) {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.left.left=new Node(40);
+        root.left.right=new Node(50);
+        root.right=new Node(30);
+        iterativePreorder(root);
+    }
 }
